1035aff0b9c6c518e3456b194cbd824dc8be764a
RiceRollRoll <bopitu@gmail.com>
2022-08-05 23:38:13 +0700

    Item Update
    added item action
    - destroy object
    -make object appear by changing the y axis
    - teleport an object to another object's location

---
+++ Assets/Scripts/Items/ItemExecuteAction.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemExecuteAction : MonoBehaviour
+{
+    public DialogueController dialogueController;
+
+    [Header("Objects to Destroy")]
+    public GameObject objectDestroy;
+
+    [Header("Objects to Appear")]
+    public GameObject objectAppear;
+    public float yAxisTransform;
+
+    [Header("Objects to Teleport")]
+    public GameObject objectTeleport;
+    public GameObject objectTeleportLocation;
+
+    private void Update()
+    {
+        if (dialogueController.DialogueEnd == true)
+        {
+            if (objectDestroy != null)
+            Destroy(objectDestroy);
+
+            if (objectAppear != null)
+            objectAppear.transform.position += new Vector3(0, yAxisTransform, 0);
+
+            if (objectTeleport != null && objectTeleportLocation != null)
+            objectTeleport.transform.position = objectTeleportLocation.transform.position;
+        }
+    }
+}

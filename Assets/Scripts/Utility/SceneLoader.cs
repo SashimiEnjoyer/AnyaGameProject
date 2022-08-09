187054940ba23a3df3924b89ad612d13e6b717d8
faris.pradiatma <faris.pradiatma@shintavr.com>
2022-08-10 00:07:22 +0700

    Add unitask

--- Assets/Scripts/Utility/SceneLoader.cs
+++ Assets/Scripts/Utility/SceneLoader.cs
@@ -1,20 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Cysharp.Threading.Tasks;
 
 public static class SceneLoader 
 {
-    public static void LoadScene(string sceneName, Action onStart = null)
+    public static async void LoadScene(string sceneName, Action onStart = null, Action<int> onProgress = null)
     {
         onStart?.Invoke();
 
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+        operation.allowSceneActivation = false;
 
         while (!operation.isDone)
         {
+            onProgress?.Invoke((int)operation.progress * 100);
             
+            if(operation.progress >= 0.9f)
+            {
+                operation.allowSceneActivation = true;
+            }
+            await UniTask.Yield();
         }
     }
 }

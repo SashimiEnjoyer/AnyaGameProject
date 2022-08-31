b0be8a93cd0eddf24c960aea020b85e379456454
RiceRollRoll <bopitu@gmail.com>
2022-08-31 12:42:40 +0700

    Main Menu 3
    escape to quit settings

--- Assets/Scripts/Main Menu/PanelMoveIn.cs
+++ Assets/Scripts/Main Menu/PanelMoveIn.cs
@@ -8,16 +8,41 @@ public class PanelMoveIn : MonoBehaviour
     public Transform panel;
     public Transform locationTarget;
     public float speed;
-    public bool useEscapeKey;
+    PanelMoveIn panelMoveIn;
+    public bool backButton;
+    private float panelX;
+    private float targetX;
+    public InputKeyInteractable keyTrigger;
+
+    void Update()
+    {
+        panelX = panel.localPosition.x;
+        targetX = locationTarget.localPosition.x;
+
+        if (panelX <= 1)
+        backButton = true;
+        
+        if (panelX == targetX)
+        backButton = false;
+
+        if (keyTrigger != null)
+        {
+            if (backButton == true)
+            keyTrigger.enabled = false;
+
+            if (backButton == false)
+            keyTrigger.enabled = true;
+        }
+    }
 
     public void MoveIn()
     {
         panel.DOMove(new Vector3(locationTarget.position.x, locationTarget.position.y, 0), 1/speed).SetUpdate(true);
     }
 
-    void Update()
+    public void BackButtonMoveIn()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && useEscapeKey == true)
+        if (backButton == true)
         MoveIn();
     }
 }
--- Assets/Scripts/Main Menu/PanelMoveInInstant.cs
+++ Assets/Scripts/Main Menu/PanelMoveInInstant.cs
@@ -7,9 +7,10 @@ public class PanelMoveInInstant : MonoBehaviour
 {
     public Transform panel;
     public Transform locationTarget;
-
+    
     public void MoveIn()
     {
         panel.position = new Vector3(locationTarget.position.x, locationTarget.position.y, 0);
     }
+
 }
